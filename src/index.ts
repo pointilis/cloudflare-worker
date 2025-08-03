1fea5e09c31d35dcb2a3542f918f1c89fdfed00c
Poralia <poraliaglobal@gmail.com>
2025-08-03 11:51:28 +0700

    test router

--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,12 @@ const app = new Hono()
 
 app.get('/', (c) => {
   return c.text('Hello Hono!')
-})
+});
 
-app.route('/gemini', gemini);
+app.get('/api', (c) => {
+  return c.text('Welcome!')
+});
+
+app.route('/api/gemini', gemini);
 
 export default app
