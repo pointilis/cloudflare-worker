dab07e51de12211941f7c4954255edb582c0b3c4
rahman <rahman@dealpos.com>
2025-09-09 16:03:00 +0700

    tasks endpoint

--- src/openai/openai.ts
+++ src/openai/openai.ts
@@ -36,7 +36,7 @@ Rules:
         model: "gpt-5-mini",
         store: false,
         response_format: {
-            type: 'json_schema',
+            "type": 'json_schema',
             "json_schema": {
                 "name": "user_schema",
                 "schema": {
@@ -66,6 +66,71 @@ Rules:
     const aiResponse = response.choices[0].message.content;
     return c.json({ 
         message: 'Milestones generated successfully', 
+        result: aiResponse ? JSON.parse(aiResponse) : null, 
+    });
+});
+
+
+// Generate tasks
+app.post('/generate-tasks', async (c) => {
+    const body = await c.req.json();
+    const { milestone, topic } = body;
+
+    const client = new OpenAI({ apiKey: c.env.OPENAI_API_KEY });
+    const prompt = `Generate 10 - 50 tasks for milestone: ${milestone}.
+Results should not be taken out the main topic: ${topic}.
+
+Rules:
+    - Result must fully expanded 10 - 50 tasks.
+    - Each task is actionable and has a clear objective.
+    - Give estimates in hours for the duration of each task.
+    - The tasks are relevant to the milestone.
+    - Make sure ordering of tasks is logical and sequential.
+    - The language of the response must be the same as language of the topic.`;
+
+    const response = await client.chat.completions.create({
+        messages: [
+            {
+                role: 'system',
+                content: `You are a helpful assistant that helps people find information about ${milestone} and ${topic}.`
+            },
+            {
+                role: 'user',
+                content: prompt
+            }
+        ],
+        model: "gpt-5-mini",
+        store: false,
+        response_format: {
+            "type": 'json_schema',
+            "json_schema": {
+                "name": "user_schema",
+                "schema": {
+                    "type": "object",
+                    "properties": {
+                        "tasks": {
+                            "type": "array",
+                            "items": {
+                                "type": "object",
+                                "properties": {
+                                    "order": {"type": "integer"},
+                                    "duration": {"type": "integer"},
+                                    "title": {"type": "string"},
+                                },
+                            },
+                            "required": ["order", "duration", "title"]
+                        }
+                    },
+                    "required": ["tasks"]
+                },
+                "strict": false
+            }
+        },
+    });
+
+    const aiResponse = response.choices[0].message.content;
+    return c.json({ 
+        message: 'Tasks generated successfully', 
         result: aiResponse ? JSON.parse(aiResponse) : null, 
     });
 });
